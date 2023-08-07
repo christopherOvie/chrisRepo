023c00dde3f413f1482e2c55ed3d61716a028836
krishna <vamankrishna123@gmail.com>
2023-08-07 17:04:08 +0530

    please check once am written String reverse program

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,5 +1,26 @@
 package tests;
 
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
 public class Test1 {
 
+	@Test
+	public void reverseString() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter you are String :");
+		String name = sc.nextLine();
+		sc.close();
+		String reverseName ="";
+		int length = name.length();
+		for(int i=length-1;i>=0;i--) {
+			reverseName = reverseName+name.charAt(i);
+		}
+		System.out.println("your String reverse is: "+reverseName);
+		
+		
+		
+	}
+	
 }
